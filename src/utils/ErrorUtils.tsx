4bb21c5bd08bad42450e1f9ed6cc3ec4a19936f0
Alireza Azhdari <alirezaazhdari11@gmail.com>
2025-11-12 17:21:03 +0330

    Commented out the error detail display in the login error message

--- src/utils/ErrorUtils.tsx
+++ src/utils/ErrorUtils.tsx
@@ -120,11 +120,11 @@ export function messageForLoginError(
             return (
                 <div>
                     <div>{_t("auth|incorrect_credentials")}</div>
-                    <div className="mx_Login_smallError">
+                    {/*<div className="mx_Login_smallError">
                         {_t("auth|incorrect_credentials_detail", {
                             hs: serverConfig.hsName,
                         })}
-                    </div>
+                    </div>*/}
                 </div>
             );
         } else {
